8f1df632dbf84bb2491d8ab5b1f6c9c17099c4e2
Entisar <Code.g.light@gmail.com>
2025-06-04 13:24:07 +0300

    ...

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,7 +3,7 @@ import HomePage from '../pages/HomePage';
 import Layout from '../layout/Layout';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
-import AdminPage from '../pages/AdminPAge';
+import AdminPage from '../pages/AdminPage';
 function AppRoutes() {
   const AppRoutes = createBrowserRouter([
     {
